0429a560440f02dd89defd13f75afbe8eac1165a
GarDanil <115405894+GarDanil@users.noreply.github.com>
2023-04-02 12:47:22 +0300

    Add files via upload

---
+++ 1/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cctype>
+#include "modAlphaCipher.h"
+#include <locale>
+#include <codecvt>
+#include <typeinfo>
+using namespace std;
+void check(const wstring& Text, const wstring& key, const bool destructCipherText=false)
+{
+    try {//Вставим обработку исключений
+        wstring cipherText;
+        wstring decryptedText;
+        modAlphaCipher cipher(key);
+        cipherText = cipher.encrypt(Text);
+        if (destructCipherText)
+            cipherText.front() = towlower(cipherText.front());
+        decryptedText = cipher.decrypt(cipherText);
+        wcout<<L"key="<<key<<endl;
+        wcout<<Text<<endl;
+        wcout<<cipherText<<endl;
+        wcout<<decryptedText<<endl;
+    } catch (const cipher_error & e) {
+        wcerr<<"Error: "<<e.what()<<endl;
+    }
+}
+
+int main()
+{
+    locale loc("ru_RU.UTF-8");
+    locale::global(loc);
+    check(L"МИР",L"КЛЮЧИК");
+    check(L"ПРИВЕТ",L"");
+    check(L"ПРИВЕТМИР",L"КЛЮЧИК123456789");
+    check(L"ПРИВЕТ МИР",L"КЛЮЧИК");
+    check(L"123456789",L"КЛЮЧИК");
+}
---
+++ 1/modAlphaCipher.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <map>
+#include <codecvt>
+#include <cstdlib>
+#include <locale>
+class modAlphaCipher
+{
+private:
+    std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> codec;
+    std::wstring numAlpha = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";//алфавит по порядку
+    std::map <wchar_t,int> alphaNum;//ассоциативный массив "номер по символу"
+    std::vector <int> key;//ключ
+    std::vector <int> convert(const std::wstring& s);//преобразование строка-вектор
+    std::wstring convert(const std::vector<int>& v);//преобразование вектор-строка
+    std::wstring getValidKey(const std::wstring & ws);
+    std::wstring getValidOpenText(const std::wstring & ws);
+    std::wstring getValidCipherText(const std::wstring & ws);
+public:
+    modAlphaCipher()=delete;//запретим конструктор без параметров
+    modAlphaCipher(const std::wstring& skey);//конструктор для установки ключа
+    std::wstring encrypt(const std::wstring& open_text);
+    std::wstring decrypt(const std::wstring& cipher_text);
+};
+class cipher_error: public std::invalid_argument
+{
+public:
+    explicit cipher_error (const std::string& what_arg):
+        std::invalid_argument(what_arg) {}
+    explicit cipher_error (const char* what_arg):
+        std::invalid_argument(what_arg) {}
+};
